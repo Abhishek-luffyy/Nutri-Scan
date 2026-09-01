96408d7c280544f5e83eb0afc38b18a77618d626
Joy Boy <abhishekinbasekar@gmail.com>
2026-09-01 16:57:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-import Home from "./Home";
-import Scanner from "./Scanner";
-import Login from "./Login";
-import Signup from "./Signup";
-import NotFound from "./NotFound";
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-export { Home, Scanner, Login, Signup, NotFound };
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/user", require("./routes/user.routes"));
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server Online" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
